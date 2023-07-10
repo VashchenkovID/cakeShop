eb230d75f03f0b1631fbc575d5c15c19d2e6ebb4
VashchenkovID <vashhenkov-ivan@mail.ru>
2023-07-10 17:22:29 +0300

    новый дизайн

--- cabinet-client/src/pages/Catalog/CatalogItem/CatalogItem.tsx
+++ cabinet-client/src/pages/Catalog/CatalogItem/CatalogItem.tsx
@@ -14,6 +14,7 @@ import cn from "classnames/bind";
 import CatalogItemPrice from "../CatalogItemPrice/CatalogItemPrice";
 import { useNavigate } from "react-router-dom";
 import { storageUser } from "src/utils/storage";
+import ComponentStyleWrapper from "src/components/ComponentStyleWrapper/ComponentStyleWrapper";
 interface IComponentProps {
   item: DeviceListModel;
   width: number;
@@ -227,74 +228,121 @@ const CatalogItem: React.FC<IComponentProps> = ({ item, width, setModal }) => {
   }, [basket]);
   return (
     <div>
-      <div
-        className={styles.Item}
-        onClick={() => navigate(`${PublicRoutesEnum.VIEW_DESSERT}/${item.id}`)}
-      >
-        <div className={styles.Item__header}>
-          <img
-            className={styles.Item__image}
-            src={`${import.meta.env.VITE_API_URL_IMAGE}${item.img}`}
-          />
-          <div className={styles.Item__title}>
-            <Text weight={"semibold"}>{item.name}</Text>
-            <div className={styles.Item__rating}>
-              <Text size={"s"} className={styles.Item__rating__text}>
-                {Math.floor(item.rating * 100) / 100}
-              </Text>
-              <IconFavorite
-                className={cx(styles.Item__rating__icon, {
-                  colored: item.rating > 0,
-                })}
-              />
+      <ComponentStyleWrapper>
+        <div
+          className={styles.Item}
+          onClick={() =>
+            navigate(`${PublicRoutesEnum.VIEW_DESSERT}/${item.id}`)
+          }
+        >
+          <div className={styles.Item__header}>
+            <img
+              className={styles.Item__image}
+              src={`${import.meta.env.VITE_API_URL_IMAGE}${item.img}`}
+            />
+            <div className={styles.Item__title}>
+              <Text weight={"semibold"}>{item.name}</Text>
+              <div className={styles.Item__rating}>
+                <Text size={"s"} className={styles.Item__rating__text}>
+                  {Math.floor(item.rating * 100) / 100}
+                </Text>
+                <IconFavorite
+                  className={cx(styles.Item__rating__icon, {
+                    colored: item.rating > 0,
+                  })}
+                />
+              </div>
             </div>
           </div>
-        </div>
-        <div className={styles.Item__basket}>
-          <CatalogItemPrice
-            price={item.price}
-            countWeightType={item.countWeightType}
-            discount={item.discount}
-          />
-          {width >= 1000 && (
-            <div className={styles.Item__button}>
-              {!isAdded ? (
-                <Button
-                  size={"xs"}
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    addItemInBasket();
-                  }}
-                  label={"Добавить"}
-                />
-              ) : (
-                <div className={styles.Item__addActions}>
-                  <Button
-                    size={"xs"}
-                    label={"-"}
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      removeItemInBasket();
-                    }}
-                  />
-                  <Text>{countItemBasket}</Text>
+          <div className={styles.Item__basket}>
+            <CatalogItemPrice
+              price={item.price}
+              countWeightType={item.countWeightType}
+              discount={item.discount}
+            />
+            {width >= 1000 && (
+              <div className={styles.Item__button}>
+                {!isAdded ? (
                   <Button
                     size={"xs"}
                     onClick={(e) => {
                       e.stopPropagation();
                       addItemInBasket();
                     }}
-                    label={"+"}
+                    label={"Добавить"}
                   />
-                </div>
-              )}
+                ) : (
+                  <div className={styles.Item__addActions}>
+                    <Button
+                      size={"xs"}
+                      label={"-"}
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        removeItemInBasket();
+                      }}
+                    />
+                    <Text>{countItemBasket}</Text>
+                    <Button
+                      size={"xs"}
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        addItemInBasket();
+                      }}
+                      label={"+"}
+                    />
+                  </div>
+                )}
+              </div>
+            )}
+          </div>
+          {width < 1000 ? (
+            <div className={styles.Item__footer}>
+              <Button
+                size={"xs"}
+                label={"Купить в 1 клик"}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  addItemInBasket();
+                  setModal(true);
+                }}
+              />
+
+              <div className={styles.Item__button}>
+                {!isAdded ? (
+                  <Button
+                    size={"xs"}
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      addItemInBasket();
+                    }}
+                    label={"Добавить"}
+                  />
+                ) : (
+                  <div className={styles.Item__addActions}>
+                    <Button
+                      size={"xs"}
+                      label={"-"}
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        removeItemInBasket();
+                      }}
+                    />
+                    <Text>{countItemBasket}</Text>
+                    <Button
+                      size={"xs"}
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        addItemInBasket();
+                      }}
+                      label={"+"}
+                    />
+                  </div>
+                )}
+              </div>
             </div>
-          )}
-        </div>
-        {width < 1000 ? (
-          <div className={styles.Item__footer}>
+          ) : (
             <Button
-              size={"xs"}
+              size={"s"}
               label={"Купить в 1 клик"}
               onClick={(e) => {
                 e.stopPropagation();
@@ -302,52 +350,9 @@ const CatalogItem: React.FC<IComponentProps> = ({ item, width, setModal }) => {
                 setModal(true);
               }}
             />
-
-            <div className={styles.Item__button}>
-              {!isAdded ? (
-                <Button
-                  size={"xs"}
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    addItemInBasket();
-                  }}
-                  label={"Добавить"}
-                />
-              ) : (
-                <div className={styles.Item__addActions}>
-                  <Button
-                    size={"xs"}
-                    label={"-"}
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      removeItemInBasket();
-                    }}
-                  />
-                  <Text>{countItemBasket}</Text>
-                  <Button
-                    size={"xs"}
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      addItemInBasket();
-                    }}
-                    label={"+"}
-                  />
-                </div>
-              )}
-            </div>
-          </div>
-        ) : (
-          <Button
-            size={"s"}
-            label={"Купить в 1 клик"}
-            onClick={(e) => {
-              e.stopPropagation();
-              addItemInBasket();
-              setModal(true);
-            }}
-          />
-        )}
-      </div>
+          )}
+        </div>
+      </ComponentStyleWrapper>
     </div>
   );
 };
