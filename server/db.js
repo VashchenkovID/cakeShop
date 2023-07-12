d9f0fd22d0e10497d0b3c946cd078478ef05dc8d
VashchenkovID <vashhenkov-ivan@mail.ru>
2023-07-12 14:19:35 +0300

    new server

--- server/db.js
+++ server/db.js
@@ -2,6 +2,6 @@ const { Sequelize } = require("sequelize");
 
 module.exports = new Sequelize("cakes", "postgres", "qwerasdfzxcv", {
   dialect: "postgres",
-  host: "188.68.221.241",
+  host: "84.38.180.242",
   port: "5432",
 });
