4cc0b0faa2a3c7de04eac9f39c2c0cbfad8af18a
VashchenkovID <vashhenkov-ivan@mail.ru>
2023-07-16 22:49:26 +0300

    git ignore

--- cabinet-client/build/pages/Catalog/CatalogItem/CatalogItem.d.ts
+++
@@ -1,9 +0,0 @@
-import React, { SetStateAction } from "react";
-import { DeviceListModel } from "src/api/models/DeviceListModel";
-interface IComponentProps {
-    item: DeviceListModel;
-    width: number;
-    setModal: React.Dispatch<SetStateAction<boolean>>;
-}
-declare const CatalogItem: React.FC<IComponentProps>;
-export default CatalogItem;
