bd8894273198c1d87dd6f83bfb927c4a3309811d
VashchenkovID <vashhenkov-ivan@mail.ru>
2023-07-16 22:06:21 +0300

    fix

---
+++ cabinet-client/build/api/requests/index.d.ts
@@ -0,0 +1,4 @@
+declare const $host: import("axios").AxiosInstance;
+declare const $authHost: import("axios").AxiosInstance;
+export declare const converterUrl: (url: string, data: any) => string;
+export { $host, $authHost };
